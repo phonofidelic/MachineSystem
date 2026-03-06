d74cdce27b35d8a49a9d9eab7e69ccf912705d1e
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-06 18:13:24 +0100

    fix dev.sh to run project on mac
    Requires local instal of Yarn

--- dev.sh
+++ dev.sh
@@ -1,5 +1,12 @@
 echo "Running tailwindcss..."
-tailwindcss -i ./MachineSystem/MachineSystem/wwwroot/app.css -o ./MachineSystem/MachineSystem/wwwroot/app.min.css --watch
+yarn dlx @tailwindcss/cli -i ./MachineSystem/MachineSystem/wwwroot/app.css -o ./MachineSystem/MachineSystem/wwwroot/app.min.css --watch[=always]
+TAILWIND_PID=$!
+
+cleanup() {
+    echo "Stopping tailwindcss..."
+    kill $TAILWIND_PID 2>/dev/null
+}
+trap cleanup EXIT
 
 echo "Starting dotnet watch..."
-dotnet --watch
+dotnet watch --project MachineSystem/MachineSystem
