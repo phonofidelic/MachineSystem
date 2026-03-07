6dfc40318acfa6b9fdfd2b0a659e03ace4e85454
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-07 03:21:14 +0100

    move machine list data to MachineList component

--- MachineSystem/MachineSystem.Client/Pages/Home.razor.cs
+++ MachineSystem/MachineSystem.Client/Pages/Home.razor.cs
@@ -1,37 +1,4 @@
 
 namespace MachineSystem.Client.Pages;
 public partial class Home
-{
-    private List<Machine> machines = [
-        new Machine
-        {
-            Name = "Machine 01",
-            Status = MachineStatus.Online,
-            LastData = "Temp: 25c"
-        },
-        new Machine
-        {
-            Name = "Machine 02",
-            LastData = "Temp: 15c"
-        }
-    ];
-}
-
-public class Machine
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    
-    public required string Name { get; set; }
-
-    public MachineStatus Status { get; set; }
-
-    public string? LastData { get; set; }
-
-    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
-}
-
-public enum MachineStatus
-{
-    Offline,
-    Online
-}
+();
