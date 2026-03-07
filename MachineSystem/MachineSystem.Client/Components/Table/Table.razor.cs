387e2785fc4eabddba15e1aa9d87c49af503873b
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-07 02:51:29 +0100

    add MachineList table components

--- MachineSystem/MachineSystem.Client/Components/Table/Table.razor.cs
+++ MachineSystem/MachineSystem.Client/Components/Table/Table.razor.cs
@@ -1,8 +1,12 @@
-using System;
+using Microsoft.AspNetCore.Components;
 
 namespace MachineSystem.Client.Components.Table;
 
 public partial class Table
 {
+    [Parameter]
+    public required RenderFragment TableHeader { get; set; }
 
+    [Parameter]
+    public required RenderFragment TableBody { get; set; }
 }
