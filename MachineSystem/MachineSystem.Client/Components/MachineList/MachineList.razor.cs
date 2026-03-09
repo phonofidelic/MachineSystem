bcb6999a3ba928d2b205f50e5871dae28235beb9
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-09 11:28:09 +0100

    add MachineListSkeleton component

--- MachineSystem/MachineSystem.Client/Components/MachineList/MachineList.razor.cs
+++ MachineSystem/MachineSystem.Client/Components/MachineList/MachineList.razor.cs
@@ -2,22 +2,18 @@
 using MachineSystem.Client.Services;
 using MachineSystem.Domain.Entities;
 using MachineSystem.Domain.Services;
-using Microsoft.AspNetCore.Components;
 
 namespace MachineSystem.Client.Components.MachineList;
 
 public partial class MachineList
 {
     private readonly IMachineService machineService = new MockClientMachineService();
-    private List<Machine> machines = [];
+    
+    private List<Machine>? machines;
 
-    [Parameter]
-    public bool IsLoading { get; set; } = true;
-
-    protected async override Task OnInitializedAsync()
+    protected override async Task OnInitializedAsync()
     {
         machines = await machineService.GetMachinesAsync();
-        IsLoading = false;
     }
 
     private async Task StartMachine(Guid machineId)
