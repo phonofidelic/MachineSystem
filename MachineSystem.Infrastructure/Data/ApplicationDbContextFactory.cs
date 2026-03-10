e1dd16856f3fe9455ed865f64d512aee3911d58e
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-10 14:06:56 +0100

    add ApplicationDbContextFactory (pseudo-code/placeholder)

---
+++ MachineSystem.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MachineSystem.Infrastructure.Data;
+
+// public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+// {
+//     public ApplicationDbContext CreateDbContext(string[] args)
+//     {
+//         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+//         optionsBuilder.UseSqlServer(...);
+//         return optionsBuilder;
+//     }
+// }
