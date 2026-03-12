3c0a59947b40e63e5386c6e1ad47877a73ccdef3
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-12 12:57:50 +0100

    make context readonly

--- MachineSystem.Infrastructure/Data/UnitOfWork.cs
+++ MachineSystem.Infrastructure/Data/UnitOfWork.cs
@@ -4,7 +4,7 @@ namespace MachineSystem.Infrastructure.Data;
 
 public class UnitOfWork(ApplicationDbContext ctx) : IUnitOfWork
 {
-    ApplicationDbContext context = ctx;
+    private readonly ApplicationDbContext context = ctx;
     public async Task SaveAsync()
     {
         await context.SaveChangesAsync();
