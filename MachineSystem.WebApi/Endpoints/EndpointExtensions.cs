15f6dbc67274409373379522b2039817eb5398f7
Christopher Clemons <chris.clemons85@gmail.com>
2026-03-15 17:50:28 +0100

    add /api/machines/create API endpoint

--- MachineSystem.WebApi/Endpoints/EndpointExtensions.cs
+++ MachineSystem.WebApi/Endpoints/EndpointExtensions.cs
@@ -1,11 +1,13 @@
+using System.Text.RegularExpressions;
 using MachineSystem.Application.Commands;
 using MachineSystem.Application.Queries;
 using MachineSystem.Application.ServiceContracts;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 // ToDo: Move to separate project
 namespace MachineSystem.WebApi.Endpoints;
 
-public static class EndpointExtensions
+public static partial class EndpointExtensions
 {
     public static IEndpointRouteBuilder MapApiEndpoints(this IEndpointRouteBuilder builder)
     {
@@ -14,52 +16,74 @@ public static class EndpointExtensions
         builder.MapGet(
             "/api/machines", 
             async (IHandler<GetMachinesQuery, GetMachinesResult> handler) => 
-        {
-            var result = await handler.HandleAsync(new GetMachinesQuery());
-            return Results.Ok(result);
-        });
+            {
+                var result = await handler.HandleAsync(new GetMachinesQuery());
+                return Results.Ok(result);
+            });
 
         builder.MapGet(
             "/api/machines/{id:Guid}", 
             async (Guid id, IHandler<GetMachineQuery, GetMachineResult> handler) => 
-        {
-            var result = await handler.HandleAsync(new GetMachineQuery(id));
-            return Results.Ok(result);
-        });
+            {
+                var result = await handler.HandleAsync(new GetMachineQuery(id));
+                return Results.Ok(result);
+            });
+
+        builder.MapPost(
+            "/api/machines/create",
+            async (CreateMachineCommand command, IHandler<CreateMachineCommand, CreateMachineResult> handler) =>
+            {
+                if (command.Name.Length < 3)
+                    return Results.BadRequest(error: "Name must contain at least 3 characters");
+
+                if (command.Name.Length > 30)
+                    return Results.BadRequest(error: "Name cannot contain more than 30 characters");
+
+                if (!AllowedMachineNameCharacters().IsMatch(command.Name))
+                    return Results.BadRequest(error: "Machine name contains disallowed characters");
+
+                logger.LogInformation("Creating new Machine");
+                var result = await handler.HandleAsync(command);
+
+                return Results.Ok(result);
+            });
 
         builder.MapPatch(
             "/api/machines/{id:Guid}/start", 
             async (Guid id, IHandler<StartMachineCommand, MachineActionResult> handler) => 
-        {
-            logger.LogInformation($"Starting machine {id}");
-            var result = await handler.HandleAsync(new StartMachineCommand(id));
-            return Results.Ok(result);
-        });
+            {
+                logger.LogInformation($"Starting machine {id}");
+                var result = await handler.HandleAsync(new StartMachineCommand(id));
+                return Results.Ok(result);
+            });
 
         builder.MapPatch(
             "/api/machines/{id:Guid}/stop", 
             async (Guid id, IHandler<StopMachineCommand, MachineActionResult> handler) => 
-        {
-            var result = await handler.HandleAsync(new StopMachineCommand(id));
-            return Results.Ok(result);
-        });
+            {
+                var result = await handler.HandleAsync(new StopMachineCommand(id));
+                return Results.Ok(result);
+            });
 
         builder.MapPatch(
             "/api/machines/{id:Guid}/connect", 
             async (Guid id, IHandler<ConnectMachineCommand, MachineActionResult> handler) => 
-        {
-            var result = await handler.HandleAsync(new ConnectMachineCommand(id));
-            return Results.Ok(result);
-        });
+            {
+                var result = await handler.HandleAsync(new ConnectMachineCommand(id));
+                return Results.Ok(result);
+            });
 
         builder.MapPatch(
             "/api/machines/{id:Guid}/disconnect", 
             async (Guid id, IHandler<DisconnectMachineCommand, MachineActionResult> handler) => 
-        {
-            var result = await handler.HandleAsync(new DisconnectMachineCommand(id));
-            return Results.Ok(result);
-        });
+            {
+                var result = await handler.HandleAsync(new DisconnectMachineCommand(id));
+                return Results.Ok(result);
+            });
 
         return builder;
     }
+
+    [GeneratedRegex(@"[A-Za-z][A-Za-z0-9\-]*")]
+    private static partial Regex AllowedMachineNameCharacters();
 }
--- MachineSystem.WebApi/ServiceExtensions.cs
+++ MachineSystem.WebApi/ServiceExtensions.cs
@@ -10,6 +10,7 @@ public static class ServiceExtensions
 {
     public static void AddScopedHandlers(this IServiceCollection services)
     {
+        services.AddScoped<IHandler<CreateMachineCommand, CreateMachineResult>, CreateMachineHandler>();
         services.AddScoped<IHandler<GetMachinesQuery, GetMachinesResult>, GetMachinesHandler>();
         services.AddScoped<IHandler<GetMachineQuery, GetMachineResult>, GetMachineHandler>();
         services.AddScoped<IHandler<StartMachineCommand, MachineActionResult>, StartMachineHandler>();
